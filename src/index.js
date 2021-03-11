e75a534b3e778e35832f5683142a2304616b7e34
Danizold34 <79314048+Danizold34@users.noreply.github.com>
2021-03-11 16:29:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
 
 exports.min = function min (array) {
-    let min = array[0];
     if (!array || array.length  === 0) return 0;
     else {
-    for( let i = 0 ; i < array.length; i++){
-        if( array[i] < array[i+1] {
-           min  = array[i];
-           }
-           
-           
-    }
-  return min;
+  return Math.min(..array);
            }
 }
 
 exports.max = function max (array) {
-                let max = array[0];
              if (!array || array.length  === 0) return 0;
-    else 
-    for( let i = 0 ; i < array.length; i++){
-        if( array[i] > array[i+1] {
-           max  = array[i];
-           }
-            
-  return max;
+    else {
+         return Math.max(..array);
            }
 }
 
 exports.avg = function avg (array) {
                 let avg = array[0];
              if (!array || array.length  === 0) return 0;
-    else 
-    for( let i = 0 ; i < array.length; i++){
-      let avg1 += array[i];
-        avg = avg1/i;
-           }
-  return avg;
-        }
+    else return (array.reduce((al, re) => al + re, 0) / array.length);
+        
 }
