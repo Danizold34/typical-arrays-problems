2e2fc5e55a010a23adacf13adeb940eade37b6f8
Danizold34 <79314048+Danizold34@users.noreply.github.com>
2021-03-11 16:31:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 
 exports.min = function min (array) {
     if (!array || array.length  === 0) return 0;
-    else {
   return Math.min(..array);
-           }
 }
 
 exports.max = function max (array) {
              if (!array || array.length  === 0) return 0;
-    else {
          return Math.max(..array);
-           }
+           
 }
 
 exports.avg = function avg (array) {
                 let avg = array[0];
              if (!array || array.length  === 0) return 0;
-    else return (array.reduce((al, re) => al + re, 0) / array.length);
+   return (array.reduce((al, re) => al + re, 0) / array.length);
         
 }
